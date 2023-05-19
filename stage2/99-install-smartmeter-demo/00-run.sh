879451d596c3793033d4df25e1b86b6745414561
Mario Coray <mario.coray@scs.ch>
2023-05-19 21:32:46 +0200

    bump smartmeter-datacollector packages to v1.1.0

--- stage2/99-install-smartmeter-demo/00-run.sh
+++ stage2/99-install-smartmeter-demo/00-run.sh
@@ -17,9 +17,9 @@ EOF
 # Download Smart Meter Data Collector
 wget \
     -O "${ROOTFS_DIR}/tmp/python3-smartmeter-datacollector.deb" \
-    "https://github.com/scs/smartmeter-datacollector/releases/download/v1.0.2/python3-smartmeter-datacollector_1.0.2-1_armhf.deb"
+    "https://github.com/scs/smartmeter-datacollector/releases/download/v1.1.0/python3-smartmeter-datacollector_1.1.0-1_armhf.deb"
 
 # Download Smart Meter Data Collector Configurator
 wget \
     -O "${ROOTFS_DIR}/tmp/python3-smartmeter-datacollector-configurator.deb" \
-    "https://github.com/scs/smartmeter-datacollector-configurator/releases/download/v1.0.2/python3-smartmeter-datacollector-configurator_1.0.2-1_armhf.deb"
+    "https://github.com/scs/smartmeter-datacollector-configurator/releases/download/v1.1.0/python3-smartmeter-datacollector-configurator_1.1.0-1_armhf.deb"
