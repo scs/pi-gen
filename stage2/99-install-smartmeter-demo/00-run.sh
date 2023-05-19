4f046a57cccbc7106fd7aa86c1631fb8f243a30c
Mario Coray <mario.coray@scs.ch>
2023-05-19 18:42:32 +0200

    fix & update influxdb & Grafana installation

--- stage2/99-install-smartmeter-demo/00-run.sh
+++ stage2/99-install-smartmeter-demo/00-run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
+on_chroot << EOF
+wget -qO- https://repos.influxdata.com/influxdata-archive_compat.key | \
+    gpg --dearmor -o /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg
+wget -qO- https://apt.grafana.com/gpg.key | \
+    gpg --dearmor -o /etc/apt/trusted.gpg.d/grafana.gpg
+EOF
+
 install -m 644 files/influxdb.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 install -m 644 files/grafana.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
-
-sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/influxdb.list"
-
-on_chroot << EOF
-wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
-wget -qO- https://packages.grafana.com/gpg.key | apt-key add -
-EOF
 
 on_chroot << EOF
 apt-get update
